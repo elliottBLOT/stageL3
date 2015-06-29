60b0babbb3f926098910861bb2e21806d307039e
elliottBLOT <elliott.blot@etu.u-bordeaux.fr>
2015-06-29 16:47:02 +0200

    version final du programme et chapitre 1 amélioré

--- graph.lisp
+++ graph.lisp
@@ -15,72 +15,87 @@
      for i from start by step
      collect i))
 
-(defun sort-nodes (list-edges)
-  (mapcar #'(lambda (x)
-	      (if (< (car x) (cadr x))
-		  (cons (car x) (cdr x))
-		  (list (cadr x) (car x) )))list-edges))
+(defun order-edge (edge)
+  (destructuring-bind (x y) edge
+    (if (< x y)
+	edge
+	(reverse edge))))
+
+(defun sort-nodes (edges)
+  (mapcar #'order-edge edges))
 
 (defun delete-double (list-edges)
   (remove-duplicates list-edges :test #'equal))
 
-(defmacro make-edges-oriented (nb-nodes exp)
-  (when (symbolp exp)
-    (setf exp (eval exp)))
+(defmacro m (nb-nodes expression)
   `(loop
-      with n = ,nb-nodes
       for i from 1 to ,nb-nodes
       nconc (loop for j from 1 to ,nb-nodes
 	       unless (= i j)
-	       when ,exp
+	       when ,expression
 	       collect (list i j))))
 
-(defmacro make-edges-unoriented (nb-nodes expression)
-  `(sort (delete-double (sort-nodes (make-edges-oriented ,nb-nodes ,expression))) #'doublon))
+(defun make-edges-oriented (nb-nodes exp)
+  (eval `(m ,nb-nodes ,exp)))
 
-(defmacro to-graph-oriented (nb-nodes expressions)
-  `(make-graph (make-edges-oriented ,nb-nodes ,expressions)
-	       (make-nodes ,nb-nodes)))
-
-(defun doublon (list1 list2)
-  (if (< (first list1) (first list2))
-      t
-      (and (= (first list1) (first list2)) (< (second list1) (second list2)))))
+(defun make-edges-unoriented (nb-nodes expression)
+  (sort
+   (delete-double
+    (sort-nodes
+     (make-edges-oriented nb-nodes expression)))
+   #'pair<))
 
 (defun make-nodes (nb-nodes)
   (iota nb-nodes 1))
 
-(defmacro to-graph-unoriented (nb-nodes expressions)
-  `(make-graph (make-edges-unoriented ,nb-nodes ,expressions) 
-	       (make-nodes ,nb-nodes)))
+(defun to-graph-oriented (nb-nodes expressions)
+  (make-graph (make-edges-oriented nb-nodes expressions)
+	      (make-nodes nb-nodes)))
 
-(defmacro main (expression test n listarg)
-  `(progn (setf n ,n)
-	  (if (eval (translate ,test ,listarg))
-	      (create ,expression ,n ,listarg)
-	      (print 'error))))
+(defun pair< (pair1 pair2)
+  (destructuring-bind (a1 b1) pair1
+    (or (< a1 b1)
+	(and (= a1 b1)
+	     (< (second pair1) (second pair2))))))
 
-(defun translate (expression listarg)
-  (do((l listarg (cdr l))
-      (e expression (nsubst (car (cdr (car l))) (car(car l)) e)))
-     ((= (length l) 0) e)))
-  
-(defmacro create (exp n listarg)
-  `(progn(translate ,exp ,listarg)
-	 (to-graph-unoriented ,n ,exp)))
+(defun to-graph-unoriented (nb-nodes expressions)
+  (make-graph (make-edges-unoriented nb-nodes expressions) 
+	      (make-nodes nb-nodes)))
 
-(defparameter *cycle* '(or (and (= i 1) (= j n)) (= j (1+ i))))
+(defun main (nb-nodes expression test &rest args &key &allow-other-keys)
+  (setq expression (copy-tree expression)) ;; for being non destructive
+  (setq test (copy-tree test)) ;; for being non destructive
+  (loop
+     while args
+     do (let ((var (pop args))
+	      (val (pop args)))
+	  (nsubst  nb-nodes 'n expression)
+	  (nsubst nb-nodes 'n test) ;; is this useful?
+	  (nsubst val var test)
+	  (nsubst val var expression)))
+  (if (eval test)
+      (to-graph-unoriented nb-nodes expression)
+      (warn "invalid test")))
+
 (defparameter *pn* '(= j (1+ i)))
 (defparameter *kn* t)
 (defparameter *empty* nil)
-(defparameter *test1* '(or (= i (+ 1 j)) (= i (* 2 j))))
-(defparameter *test2* '(or (= j (+ 1 i)) (= j (* 2 i))))
-(defparameter *test3* '(or (= i (- j 1)) (= j (* 2 i))))
-(defparameter *not* '(not (or (= i (+ 1 j)) (= i (* 2 j)))))
-(defparameter *g*      '(or (= (+ 3  j) i) (and (= i (1+ j)) (/= (rem j 3 ) 0))))
-(defparameter *grille* '(or (= (+ x0 j) i) (and (= i (1+ j)) (/= (rem j x0) 0)))) 
+(defparameter *cycle* 
+  '(or (and (= i 1) (= j n)) (= j (1+ i))))
+(defparameter *test1* 
+  '(or (= i (+ 1 j)) (= i (* 2 j))))
+(defparameter *test2* 
+  '(or (= j (+ 1 i)) (= j (* 2 i))))
+(defparameter *test3* 
+  '(or (= i (- j 1)) (= j (* 2 i))))
+(defparameter *not* 
+  '(not (or (= i (+ 1 j)) (= i (* 2 j)))))
+(defparameter *g*      
+  '(or (= (+ 3 j) i) (and (= i (1+ j)) (/= (rem j 3 ) 0))))
+(defparameter *grille* 
+  '(or (= (+ :x0 j) i) (and (= i (1+ j)) (/= (rem j :x0) 0)))) 
 
-(defparameter *test-grille* '(= 0 (rem n x0)))
+(defparameter *test-grille* '(= 0 (rem n :x0)))
 
-(defparameter *testranslate* '( x0 x1 x2 x3 x4 x5 x6 x8 x7 x9 x5 x4))
-
+;; Example
+;; (main 15 *grille* *test-grille* :x0 3) => [15 nodes 22 edges]
